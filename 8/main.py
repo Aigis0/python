03db2e7bb78ba6e0185079adab4675ad591855b4
Muhammad Bashir Umar <muhdbashir261@gmail.com>
2023-08-23 13:25:45 +0100

    python

---
+++ 8/main.py
@@ -0,0 +1,21 @@
+#def greet():
+#    print("Good Morning")
+#    print("Good Afternoon")
+#    print("Good Evening")
+
+#greet()
+
+#def greet_with_name(name):
+#    print(f"Good Morning {name}")
+#    print(f"Good Afternoon {name}")
+#    print(f"Good Evening {name}")
+
+#greet_with_name("barshow")
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what's it like in {location}")
+greet_with("barshow", "kaduna")
+def greet(input):
+    a = input("what is your name: ")
+    print(f"hello {a}")
+greet(input)
